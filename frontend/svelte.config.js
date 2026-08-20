bdaed609d755bd858b9f77edc3978b63b9850ce2
Korinn Clark <korinn.clark17@stjohns.edu>
2026-08-20 10:54:54 +0000

    doc

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,13 +2,7 @@ import preprocess from 'svelte-preprocess';
 
 /** Minimal Svelte config for Vite (not SvelteKit) */
 const config = {
-    preprocess: preprocess({ postcss: true }),
-    compilerOptions: {
-        // Keep the old `new Component()` API compatibility for existing code
-        compat: {
-            componentApi: 4
-        }
-    }
+    preprocess: preprocess({ postcss: true })
 };
 
 export default config;
